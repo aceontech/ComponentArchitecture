a7798fa41970403b0401a83e19e83d073980d56b
Alex Manarpies <alex@jarroo.com>
2018-03-04 22:49:23 +0100

    Move demo

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 import ComponentArchitecture
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let container = UIKitComponentController(with:
-            CustomComponentA(props: ())
+            CustomComponent(props: ())
         )
         addChildViewController(container)
         view.addSubview(container.view)
+    }
+}
+
+class CustomComponent: Component {
+    typealias State = Void
+    required init(props: Void) {}
+
+    func render() -> UIView {
+        return .stack(children: [
+            .label(.init(text: "Label 1", backgroundColor: .yellow)),
+            .stack(.init(axis: .horizontal), children: [
+                .label(.init(text: "Label 2", backgroundColor: .cyan)),
+                .label(.init(text: "Label 3", backgroundColor: .orange))
+            ])
+        ])
     }
 }
